e866234cf7089a8419b2d1bdcb8ee52f927be6e6
yuna <heoyuna111@gmail.com>
2026-02-11 02:19:58 +0900

    before fix

--- src/contents/ServiceInfo/ServiceInfo.jsx
+++ src/contents/ServiceInfo/ServiceInfo.jsx
@@ -3,6 +3,9 @@ import './ServiceInfo.css';
 import servicePpt from "../../assets/images/servicePpt.png";
 import 기준1 from "../../assets/images/기준1.png";
 import 기준2 from "../../assets/images/기준2.png";
+import model from "../../assets/images/yolo_model_description.png"
+import data from "../../assets/images/dataset.png"
+import used from "../../assets/images/model_used.png"
 
 
 
@@ -34,12 +37,38 @@ const ServiceInfo = () => {
       
       <div className="info-section">
         <h3 className="section-label">정책기준 정의</h3>
+
       <div className="policy-column">
         <div className="policy-item">
           <img src={기준1} alt="Policy 1" className="policy-full-img" />
         </div>
         <div className="policy-item">
           <img src={기준2} alt="Policy 2" className="policy-full-img" />
+        </div>
+
+
+        <h3 className="section-label">모델 선정</h3>
+        <div className="policy-item">
+          <img src={used} alt="Policy 2" className="policy-full-img" />
+        </div>
+
+
+        <h3 className="section-label">데이터 전처리</h3>
+        <div className="policy-item">
+          <img src={data} alt="Policy 2" className="policy-full-img" />
+        </div>
+        <p className="section-desc">
+          Owlv2 자동 라벨링을 통해 초기 데이터 확보<br />
+          정제단계에서 <strong>Label Studio</strong>사용<br />
+          False Negative 제거(오탐지, 객체가 없는 위치임에도 불구하고 라벨링된 경우)<br />
+          False Positive 확인(누락, 객체가 있음에도 인식되지 않은 경우)<br />
+          MisLavel 수정(잘못 라벨링된 클래스 수정, 침대를 소파로 라벨링한 경우)
+        </p>
+
+
+        <h3 className="section-label">YOLO Model Description</h3>
+        <div className="policy-item">
+          <img src={model} alt="Policy 2" className="policy-full-img" />
         </div>
       </div>
     </div>
